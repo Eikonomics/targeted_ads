20347330826e03cd3cbc751c38581276d74b1a1d
Eirikur Ragnarsson <43515589+Eikonomics@users.noreply.github.com>
2023-12-15 10:22:30 +0000

    Add files via upload

---
+++ 1_reverse_engineered_ad_algorithm.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(imager)
+
+plot(0)
+
+# input data from cookies
+did_eiki_buy_this_item_like_very_very_recently = "no"
+is_this_a_durable_good = "no"
+proabability_that_he_will_need_another_one_of_exaactly_this_item_in_the_near_future = 1
+
+# intelligent algorithm to determine whether or not to display ad to internet user "eiki"
+if (did_eiki_buy_this_item_like_very_very_recently == "yes") {
+  if (is_this_a_durable_good == "yes") {
+    
+    print("Whatevs")
+  }
+  if (proabability_that_he_will_need_another_one_of_exaactly_this_item_in_the_near_future >= 0) {
+    
+    print("hee")
+  }
+  
+  plot(load.image("target.PNG"))
+} else {
+  
+  plot(load.image("target.PNG"))
+}
+
+# end
